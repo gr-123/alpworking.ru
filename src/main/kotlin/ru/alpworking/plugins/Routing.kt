fa6ecb4e78538dc1ea7cb4a8002bc5fa9cbd0b56
Your Name <you@example.com>
2022-07-09 14:40:48 +0300

    Развертывание на сервере.

--- src/main/kotlin/ru/alpworking/plugins/Routing.kt
+++ src/main/kotlin/ru/alpworking/plugins/Routing.kt
@@ -1,19 +1,48 @@
 package ru.alpworking.plugins
 
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 //import io.ktor.http.*
-import io.ktor.server.http.content.*
-import io.ktor.server.application.*
 //import io.ktor.server.response.*
 //import io.ktor.server.request.*
 
 fun Application.configureRouting() {
     
-    // http://localhost:8080/static/ktor_logo.png
-    // http://localhost:8080/static/aboutme.html
     routing {
-        static("/static") {
-            resources("files")
+            
+            // Обслуживание статического контента
+            // https://ktor.io/docs/serving-static-content.html
+            
+            static("/") {
+                
+                // установить static пакет по умолчанию (внутри resources папки)
+                staticBasePackage = "static"
+        
+                // рекурсивно обслуживать все файлы из static папки, если URL-адрес и физическое имя файла совпадают
+                // resources(".")
+        
+                // обслуживание отдельных файлов
+                resource("index.html") // 'static/index.html'
+
+                // определить ресурс по умолчанию
+                defaultResource("index.html") // '/' -> 'static/index.html'
+        
+                // определить подмаршруты по URL-адресу
+                static("images") { // '/images'
+
+                    resource("ktor_logo.png")              // '/images/ktor_logo.png'
+                    resource("image.png", "ktor_logo.png") // '/images/ktor_logo.png' | '/images/image.png'
+                }
+
+                static("assets") {
+
+                    // обслуживание содержимого из папки ресурсов
+                    // позволит любому файлу, расположенному в 'css' папке ресурсов, служить статическим содержимым по заданному шаблону URL
+                    resources("css") // '/assets/styles.css' -> 'files/css/styles.css'
+                    resources("js")
+                }
         }
     }
+
 }
