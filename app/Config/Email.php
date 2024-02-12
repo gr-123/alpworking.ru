e7b9de862b18a165e63ce88cf7258b3478efb09f
Your Name <you@example.com>
2024-02-12 17:55:52 +0300

    Начальная настройка Ccodeigniter4 Shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'support@alpworking.ru';
+    public string $fromName   = 'support';
     public string $recipients = '';
 
     /**
